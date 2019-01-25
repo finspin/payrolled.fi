0e928ab9b00fd37e2683c71b8a20608f466d5530
finspin <jarospisak@gmail.com>
2019-01-25 21:46:54 +0200

    xrange renamed to range in Python 3

--- scripts/get-jobs.py
+++ scripts/get-jobs.py
@@ -20,7 +20,7 @@ def generate_jobs_json():
 # https://github.com/boto/boto3/issues/369
 def replace_decimals(obj):
     if isinstance(obj, list):
-        for i in xrange(len(obj)):
+        for i in range(len(obj)):
             obj[i] = replace_decimals(obj[i])
         return obj
     elif isinstance(obj, dict):
